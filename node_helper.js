90b3bee4c3777de980c13415bc62d8d8af2f4519
MichaelByers <michael.byers@comcast.net>
2023-06-19 17:06:39 -0600

    added catch

--- node_helper.js
+++ node_helper.js
@@ -20,21 +20,24 @@ module.exports = NodeHelper.create({
 
 		axios.get(this.url)
 			.then(function (response) {
-            // Lets convert the body into JSON
-            var forecast = []; // Clear the array
+				// Lets convert the body into JSON
+				var forecast = []; // Clear the array
 
-            // Check to see if we are error free and got an OK response
-            if (response.status == 200) {
-                forecast = response.data;
-            } else {
-                // In all other cases it's some other error
-                console.log('[MMM-Powder] ' + response.status);
-            }
+				// Check to see if we are error free and got an OK response
+				if (response.status == 200) {
+					forecast = response.data;
+				} else {
+					// In all other cases it's some other error
+					console.log('[MMM-Powder] ' + response.status);
+				}
 
-            // We have the response figured out so lets fire off the notifiction
-            _this.sendSocketNotification('GOT-POWDER-FORECAST', {'url': _this.url, 'forecast': forecast});
-        });
-    },
+				// We have the response figured out so lets fire off the notifiction
+				_this.sendSocketNotification('GOT-POWDER-FORECAST', {'url': _this.url, 'forecast': forecast});
+        	})
+			.catch(function (error) {
+				console.log('[MMM-Powder] ** ERROR ** : ' +error);
+		});
+	},
 
     socketNotificationReceived: function(notification, payload) {
         // Check this is for us and if it is let's get the weather data
